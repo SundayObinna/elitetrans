4ee65fdca10fd35dbad37bab7a55aba862fd8aa8
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-12 12:26:22 +0100

    Provisioned 'Carousel'.

---
+++ src/components/Carousel.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../assets/css/Carousel.css";
+
+export default function Carousel() {
+  // const noRef = null;
+
+  return (
+    <>
+      <h1 className="p-5 text-center">Carousel Slider</h1>
+    </>
+  );
+}
