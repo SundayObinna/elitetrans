3d8040d1c0c19f7d28d06dc53df21588271fe3fb
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-13 20:23:30 +0100

    Made BG Image cover background and no-repeat.

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,18 @@ export default function Header() {
         <div className="flex-grow-0 ">
           <Navbar />
         </div>
-        <div className="flex-grow-1">
+        <div
+          className="flex-grow-1"
+          style={{
+            backgroundImage:
+              "url('https://fastwpdemo.com/newwp/ambons/wp-content/uploads/2022/02/slide-v1-2.jpg')",
+            backgroundColor: "rgba(0, 0, 0, 0.7)",
+            backgroundPosition: "center center",
+            backgroundBlendMode: "saturation",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        >
           <Jumbotron />
         </div>
       </section>
