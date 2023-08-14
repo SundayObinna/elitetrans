30a6d53064b62d851e57c6562b5d84ba857fdf6f
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-14 15:18:00 +0100

    SpecializedService.js

--- src/components/SpecializedService.js
+++ src/components/SpecializedService.js
@@ -3,7 +3,17 @@ import "../App.css";
 export default function SpecializedService() {
   return (
     <section className="specialized">
-      <h1 className="text-center py-5">Specialized Service</h1>
+      <div
+        className="about-bg py-5"
+        style={{
+          backgroundImage:
+            "linear-gradient(45deg, var(--et-color-1), var(--et-color-2))",
+          color: "whitesmoke",
+        }}
+      >
+        <h1 className="text-center py-5 my-5">Specialized Care Service</h1>
+      </div>
+      <div className="container"></div>
     </section>
   );
 }
