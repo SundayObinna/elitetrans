d913aed7ac5e6566929b8a3e63d6531d46523608
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-14 14:19:46 +0100

    Improved: 'AmbulatoryService.js'.

---
+++ src/components/AmbulatoryService.js
@@ -0,0 +1,9 @@
+import "../App.css";
+
+export default function AmbulatoryService() {
+  return (
+    <section className="ambulatory">
+      <h1 className="text-center py-5">Ambulatory Service</h1>
+    </section>
+  );
+}
