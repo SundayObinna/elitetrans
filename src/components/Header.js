fb5faad3e3ad563ac19f85bf5c1d9cf973322b20
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-14 14:17:59 +0100

    Improved: 'Header.js'.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,10 @@
 import Navbar from "./Navbar";
-import Jumbotron from "./Jumbotron";
 
 export default function Header() {
   return (
     <>
-      <section className="welcome d-flex flex-column vh-100">
-        <div className="flex-grow-0 ">
-          <Navbar />
-        </div>
-        <div
-          className="flex-grow-1"
-          style={{
-            backgroundImage:
-              "url('https://fastwpdemo.com/newwp/ambons/wp-content/uploads/2022/02/slide-v1-2.jpg')",
-            backgroundColor: "rgba(0, 0, 0, 0.7)",
-            backgroundPosition: "center center",
-            backgroundBlendMode: "saturation",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-          }}
-        >
-          <Jumbotron />
-        </div>
+      <section className="welcome d-flex">
+        <Navbar />
       </section>
     </>
   );
