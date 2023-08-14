f722551abc346195ea571ca539641cb86008d658
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-14 15:15:07 +0100

    AmbulatporyService.js

--- src/components/AmbulatoryService.js
+++ src/components/AmbulatoryService.js
@@ -3,7 +3,17 @@ import "../App.css";
 export default function AmbulatoryService() {
   return (
     <section className="ambulatory">
-      <h1 className="text-center py-5">Ambulatory Service</h1>
+      <div
+        className="about-bg py-5"
+        style={{
+          backgroundImage:
+            "linear-gradient(45deg, var(--et-color-1), var(--et-color-2))",
+          color: "whitesmoke",
+        }}
+      >
+        <h1 className="text-center py-5 my-5">Ambulatory Service</h1>
+      </div>
+      <div className="container"></div>
     </section>
   );
 }
