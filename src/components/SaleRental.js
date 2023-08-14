556e7457c49dcf806352af41d03e557dbbfac537
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-14 14:18:42 +0100

    Improved: 'SaleRental.js'.

---
+++ src/components/SaleRental.js
@@ -0,0 +1,9 @@
+import "../App.css";
+
+export default function SaleRental() {
+  return (
+    <section className="sale-rental">
+      <h1 className="text-center py-5">Sale Rental</h1>
+    </section>
+  );
+}
