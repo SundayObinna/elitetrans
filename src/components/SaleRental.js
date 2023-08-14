6e099533a06aadcf9be72c5ec8d6557c98eff297
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-14 15:18:44 +0100

    SaleRental.js

--- src/components/SaleRental.js
+++ src/components/SaleRental.js
@@ -3,7 +3,17 @@ import "../App.css";
 export default function SaleRental() {
   return (
     <section className="sale-rental">
-      <h1 className="text-center py-5">Sale Rental</h1>
+      <div
+        className="about-bg py-5"
+        style={{
+          backgroundImage:
+            "linear-gradient(45deg, var(--et-color-1), var(--et-color-2))",
+          color: "whitesmoke",
+        }}
+      >
+        <h1 className="text-center py-5 my-5">Vehicle Sale/Rental</h1>
+      </div>
+      <div className="container"></div>
     </section>
   );
 }
