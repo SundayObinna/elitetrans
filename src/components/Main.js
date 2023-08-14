386eed9d9c7466f0a6f4868f9a662c9ee1a12eef
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-14 08:49:33 +0100

    Improved: 'Nain.js'.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,18 @@
 import "../App.css";
+import AboutService from "./AboutService";
+import MedicalService from "./MedicalService";
+import ServiceHighlight from "./ServiceHighlight";
+import Trusted from "./Trusted";
 
 export default function Main() {
   return (
     <>
-      <h1 className="text-center">Main</h1>
+      <section className="container py-5">
+        <MedicalService />
+        <ServiceHighlight />
+        <AboutService />
+      </section>
+      <Trusted />
     </>
   );
 }
