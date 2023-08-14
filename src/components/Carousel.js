43ae4366c09511de503b40d6fa13a34e8e92b716
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-14 08:47:48 +0100

    Carousel.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,33 @@
 import "../assets/css/Carousel.css";
 
 export default function Carousel() {
-  // const noRef = null;
-
   return (
-    <>
-      <h1 className="p-5 text-center">Carousel Slider</h1>
-    </>
+    <section>
+      <base href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/" />
+      <div id="captioned-gallery">
+        <figure className="slider">
+          <figure>
+            <img src="hobbiton-lake.jpg" alt="Slider" />
+            <figcaption>Hobbiton, New Zealand</figcaption>
+          </figure>
+          <figure>
+            <img src="wanaka-drowned-tree.jpg" alt="Slider" />
+            <figcaption>Wanaka, New Zealand</figcaption>
+          </figure>
+          <figure>
+            <img src="utah-peak.jpg" alt="Slider" />
+            <figcaption>Utah, United States</figcaption>
+          </figure>
+          <figure>
+            <img src="bryce-canyon-utah.jpg" alt="Slider" />
+            <figcaption>Bryce Canyon, Utah, United States</figcaption>
+          </figure>
+          <figure>
+            <img src="hobbiton-lake.jpg" alt="Slider" />
+            <figcaption>Hobbiton, New Zealand</figcaption>
+          </figure>
+        </figure>
+      </div>
+    </section>
   );
 }
