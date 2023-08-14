1022dba845934520fd28f75fd8bdc9f1ecae7d1a
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-14 08:48:56 +0100

    Improved: 'Jumbotron.js'.

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,12 +1,20 @@
 import "../App.css";
-import Carousel from "./Carousel";
 
 export default function Jumbotron() {
-  // const noRef = null;
-
   return (
     <>
-      <Carousel />
+      <div className="container-fluid d-flex h-100 justify-content-center align-items-center text-center">
+        <div className="row">
+          <div className="col-12">
+            <div>
+              <h1 className="text-light m-0 p-0">eliteTrans</h1>
+              <small className="text-light m-0 p-0">
+                On time, all the time...
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
