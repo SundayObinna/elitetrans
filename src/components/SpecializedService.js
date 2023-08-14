0983b9d28654a0b3e2aa3062868700221782ef8a
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-14 14:19:18 +0100

    Improved: 'SpecializedService.js'.

---
+++ src/components/SpecializedService.js
@@ -0,0 +1,9 @@
+import "../App.css";
+
+export default function SpecializedService() {
+  return (
+    <section className="specialized">
+      <h1 className="text-center py-5">Specialized Service</h1>
+    </section>
+  );
+}
