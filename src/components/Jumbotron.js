938ef750a14f2f46054e1575142dbdb2f1c17946
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-14 14:18:19 +0100

    Improved: 'Jumbotron.js'.

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -3,7 +3,18 @@ import "../App.css";
 export default function Jumbotron() {
   return (
     <>
-      <div className="container-fluid d-flex h-100 justify-content-center align-items-center text-center">
+      <div
+        style={{
+          backgroundImage:
+            "url('https://fastwpdemo.com/newwp/ambons/wp-content/uploads/2022/02/slide-v1-2.jpg')",
+          backgroundColor: "rgba(0, 0, 0, 0.7)",
+          backgroundPosition: "center center",
+          backgroundBlendMode: "saturation",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+        className="container-fluid d-flex vh-90 h-100 justify-content-center align-items-center text-center p-5 mt-4"
+      >
         <div className="row">
           <div className="col-12">
             <div>
