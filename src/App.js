cb6d23a98284d2ee64e46dd8d82b49b74e370910
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-14 14:58:09 +0100

    Improved: 'App.js'.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import "./App.css";
-import Header from "./components/Header";
+
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import Services from "./components/Services";
+import AmbulatoryService from "./components/AmbulatoryService";
+import SpecializedService from "./components/SpecializedService";
+import SaleRental from "./components/SaleRental";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import PageNotFound from "./components/PageNotFound";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <Navbar />
+
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/services" element={<Services />} />
+        <Route exact path="/ambulatory" element={<AmbulatoryService />} />
+        <Route exact path="/specialized" element={<SpecializedService />} />
+        <Route exact path="/sale-rental" element={<SaleRental />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+
+        <Route exact path="*" element={<PageNotFound />} />
+      </Routes>
+
       <Footer />
     </div>
   );
